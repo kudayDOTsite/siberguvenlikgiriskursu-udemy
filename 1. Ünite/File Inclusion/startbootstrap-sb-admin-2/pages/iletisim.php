84d938d6d4ded3c54316ea64a7a2d6052efd0e58
Beren Kuday GÖRÜN <kuday.site@gmail.com>
2020-04-09 23:29:19 -0400

    File Inclusion Projesi Eklendi

---
+++ 1. Ünite/File Inclusion/startbootstrap-sb-admin-2/pages/iletisim.php
@@ -0,0 +1,10 @@
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">İletişim Sayfası</h1>
+
+          <div class="row">
+            <div class="col-md-12">
+              <p>İçerik</p>
+            </div>
+          </div>
+          
